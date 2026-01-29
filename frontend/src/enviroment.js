9ea6a09235c957dcf879fc77cb9e52694aa9aa0b
Rakesh Kushwaha <rakeshkk589@gmail.com>
2026-01-29 15:20:27 +0530

    Deploying frontend

--- frontend/src/enviroment.js
+++ frontend/src/enviroment.js
@@ -1,5 +1,14 @@
 // let IS_PROD = true;
-const server = 
+// const server = 
+
+//     "http://localhost:8000"
+
+
+// export default server;
+
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://talkspace-3wp3.onrender.com" :
 
     "http://localhost:8000"
 
